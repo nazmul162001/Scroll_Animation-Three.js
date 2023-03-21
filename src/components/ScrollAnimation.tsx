8e814e02d22fcde2d8e32963e578541bc2ba5117
Nazmul Hasan <nazmulprime162001@gmail.com>
2023-03-21 09:19:43 +0600

    added material & texture load

--- src/components/ScrollAnimation.tsx
+++ src/components/ScrollAnimation.tsx
@@ -32,9 +32,15 @@ const ScrollAnimation = () => {
     /**
      * Objects
      */
+    // Texture
+    const textureLoader = new THREE.TextureLoader()
+    const gradientTexture = textureLoader.load('textures/gradients/3.jpg')
+    gradientTexture.magFilter = THREE.NearestFilter
+
     // Material
     const material = new THREE.MeshToonMaterial({
       color: parameters.materialColor,
+      gradientMap: gradientTexture,
     })
 
     // Meshes
